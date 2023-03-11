28eaf5b0c83e9930fcc071a0a73df2f0c24d36bb
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-03-11 21:06:07 +0200

    Create and render a browser router in app.js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div className="App">
+      <div>navbar</div>
+      <Outlet/>
+      <div>footer</div>
+    </div>
+  )
+}
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+        element: <div>Home Page</div>,
+      },
+      {
+        path: "/products/:id",
+        element: <div>Category Page</div>,
+      },
+      {
+        path: "/product/:id",
+        element: <div>Product Page</div>,
+      },
+    ],
+  },
+]);
+
+
 function App() {
   return (
-    <div className="App"></div>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
