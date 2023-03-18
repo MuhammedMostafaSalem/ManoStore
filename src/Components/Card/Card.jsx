c5e1d267dbe8c158b0e603a79519c2603b7e6cf1
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-03-18 11:44:00 +0200

    Create Card component

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Card.css'
+import { Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+const Card = ({item}) => {
+    return (
+        <Col lg='3' md='6' sm='12'>
+            <Link className='link' to={`/products/${item.id}`}>
+                <div className='card'>
+                    <div className='image'>
+                        {item.isNew  && <span>New Season</span>}
+                        <img src={item.img} className='mainImg' alt=''/>
+                        <img src={item.img2} className='secondImg' alt=''/>
+                    </div>
+                    <div className='cardBody'>
+                        <h2>{item.title}</h2>
+                        <div className='prices'>
+                            <h3>{item.oldPrice}</h3>
+                            <h3>{item.price}</h3>
+                        </div>
+                    </div>
+                </div>
+            </Link>
+        </Col>
+    )
+}
+
+export default Card
