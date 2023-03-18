7c9fe9245a73e67011ebccc76a7ca8213c38bcc9
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-03-18 11:45:38 +0200

    Add FeaturedProducts Component in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import FeaturedProducts from '../../Components/Featured Products/FeaturedProducts'
 import Slider from '../../Components/Slider/Slider'
 
 const Home = () => {
     return (
         <div className='home'>
             <Slider/>
+            <FeaturedProducts />
         </div>
     )
 }
