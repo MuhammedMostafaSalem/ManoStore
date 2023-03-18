6ef623ba02a10949a98081cf8fd70febf70cad9c
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-03-18 12:00:50 +0200

    Add Subtitle component in home  featured_products

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import FeaturedProducts from '../../Components/Featured Products/FeaturedProducts'
 import Slider from '../../Components/Slider/Slider'
+import Subtitle from '../../Components/Utility/Subtitle/Subtitle'
 
 const Home = () => {
     return (
         <div className='home'>
             <Slider/>
-            <FeaturedProducts />
+            <div className='featured_products'
+                style={{
+                    marginTop: '100px',
+                    padding: '0 150px'
+                }}
+            >
+                <Subtitle
+                    title='featured products'
+                    decription= "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas."
+                />
+                <FeaturedProducts />
+            </div>
         </div>
     )
 }
