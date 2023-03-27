333650369ff3b9f70f6507c37515042d3c1e7fd5
MuhammedMostafaSalem <109612447+MuhammedMostafaSalem@users.noreply.github.com>
2023-03-27 13:25:19 +0200

    rest filter api

--- src/Components/Card/CardContainer.jsx
+++ src/Components/Card/CardContainer.jsx
@@ -1,46 +1,20 @@
 import React from 'react'
 import { Row } from 'react-bootstrap';
+import useFetch from '../../Hooks/useFetch';
 import Card from '../Card/Card';
 
-const CardContainer = () => {
+const CardContainer = ({catId, maxPrice, sort, subCats}) => {
     
-    const data = [
-        {
-            id: 1,
-            img: 'https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600',
-            img2: 'https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600',
-            title: 'Long Sleeve Graphic Tishirt',
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 2,
-            img: 'https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600',
-            title: 'Coat',
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 3,
-            img: 'https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress&cs=tinysrgb&w=1600',
-            title: 'Skirt',
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 4,
-            img: 'https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600',
-            title: 'Hat',
-            oldPrice: 19,
-            price: 12,
-        },
-    ]
+    const {data, loading, error} = useFetch(
+        `/products?populate=*&[filters][categories][id]=${catId}${subCats.map(item => `&[filters][sub_categories][id][$eq]=${item}`)}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
+    );
+
     return (
         <div className='list'>
             <Row>
                 {
+                    loading ?
+                    "loading ..." :
                     data.map((item)=> {
                         return(
                             <Card item={item} key={item.id}/>
